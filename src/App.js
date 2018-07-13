00c98882f28aeaa2472c5e95e4934d5d1ba83fa8
Fikri Karim <fkfikrikarim@gmail.com>
2018-07-13 10:49:37 +0700

    Initial Commit.

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import { Webcam } from "./webcam";
+import classes from "./classes";
+
+let webcam;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      prediction: "",
+      confidence: ""
+    };
+  }
+  componentDidMount() {
+    webcam = new Webcam(document.getElementById("webcam"));
+    this.init();
+  }
+
+  async init() {
+    try {
+      await webcam.setup();
+    } catch (e) {
+      console.log("no webcam");
+    }
+    this.mobilenet = await this.loadMobilenet();
+
+    // Warm up the model. This uploads weights to the GPU and compiles the WebGL
+    // programs so the first time we collect data from the webcam it will be
+    // quick.
+    tf.tidy(() => this.mobilenet.predict(webcam.capture()));
+
+    setInterval(() => {
+      this.predict();
+    }, 2000);
+  }
+
+  async predict() {
+    const predictedClass = tf.tidy(() => {
+      // Capture the frame from the webcam.
+      const img = webcam.capture();
+
+      // Make a prediction through mobilenet, getting the internal activation of
+      // the mobilenet model.
+      const prediction = this.mobilenet.predict(img);
+
+      // Returns the index with the maximum probability. This number corresponds
+      // to the class the model thinks is the most probable given the input.
+      return prediction;
+    });
+
+    const index = await predictedClass
+      .as1D()
+      .argMax()
+      .data();
+    const confidence = await predictedClass.as1D().data();
+
+    console.log(classes[index]);
+    this.setState({
+      prediction: classes[index],
+      confidence: confidence[index]
+    });
+
+    predictedClass.dispose();
+  }
+
+  async loadMobilenet() {
+    const modelURL = "/vgg16/model.json";
+    const mobilenet = await tf.loadModel(modelURL);
+
+    return mobilenet;
+    // Return a model that outputs an internal activation.
+    // const layer = mobilenet.getLayer("conv_pw_13_relu");
+    // return tf.model({ inputs: mobilenet.inputs, outputs: layer.output });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <video
+          capture
+          name="video"
+          autoPlay
+          playsInline
+          muted
+          id="webcam"
+          width="100"
+          height="100"
+          capture="environment"
+        />
+        {!this.state.prediction && <h2>Loading the model..</h2>}
+        <h1>{this.state.prediction}</h1>
+        {this.state.prediction && (
+          <h2>Confidence: {this.state.confidence * 100}%</h2>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
